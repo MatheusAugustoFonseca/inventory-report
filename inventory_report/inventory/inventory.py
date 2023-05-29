21675dd92044482555944ad0ac842d238b305090
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 03:29:14 -0300

    req 6 - with xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,18 +2,31 @@ import csv
 import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
     @staticmethod
     def import_data(file_path, report_type):
+
         if ".csv" in file_path:
             with open(file_path, encoding="utf8") as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 data = [row for row in csv_reader]
+
         if ".json" in file_path:
             with open(file_path) as json_file:
                 data = json.load(json_file)
+
+        if ".xml" in file_path:
+            with open(file_path) as xml_file:
+                elem_tree = ET.parse(xml_file)
+                root = elem_tree.getroot()
+                data = [
+                    {elem.tag: elem.text for elem in record}
+                    for record in root.findall("record")
+                ]
+
         if report_type == "simples":
             return SimpleReport.generate(data)
         return CompleteReport.generate(data)
