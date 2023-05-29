0bfd7d3f04a5b691cf4dd060f2bf148985ebe1fd
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 01:06:40 -0300

    req 2 - gerar versao simpli do report

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products):
+        production_dates = list()
+        expiration_dates = list()
+        companies = list()
+
+        for product in products:
+            production_date = product.get("data_de_fabricacao")
+            expiration_date = product.get("data_de_validade")
+            expiration_date_obj = datetime.strptime(
+                expiration_date, "%Y-%m-%d"
+            ).date()
+            company = product.get("nome_da_empresa")
+            production_dates.append(production_date)
+            if expiration_date_obj >= str(datetime.now().date()):
+                expiration_dates.append(expiration_date_obj)
+            companies.append(company)
+
+            earliest_date = min(production_dates)
+            latest_expiry_date = min(production_dates)
+            most_products_company = Counter(companies).most_common(1)[0][0]
+            report = (
+                f"Data de fabricação mais antiga: {earliest_date}\n"
+                f"Data de fabricação mais próxima: {latest_expiry_date}\n"
+                f"Empresa com mais produtos: {most_products_company}\n"
+            )
+            return report
