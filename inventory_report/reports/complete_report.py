e03936636a546dff3de28d8c7219ad7fff0b5a7a
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 02:33:18 -0300

    req 3 - gerar full report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,16 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(products):
+        report = SimpleReport.generate(products)
+
+        companies_stocks = Counter(
+            product["nome_da_empresa"] for product in products
+        )
+        report += "\nProdutos estocados por empresa:\n"
+        for company, count in companies_stocks.items():
+            report += f"- {company}: {count}\n"
+        return report
