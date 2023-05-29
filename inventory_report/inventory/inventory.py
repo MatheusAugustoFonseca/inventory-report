fb5c0ec7ea3637584bd30409f1771911bef403c9
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 02:56:14 -0300

    req 5

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,15 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @staticmethod
+    def import_data(file_path, report_type):
+        if ".csv" in file_path:
+            with open(file_path, encoding="utf8") as csv_file:
+                csv_reader = csv.DictReader(csv_file)
+                data = [row for row in csv_reader]
+        if report_type == "simples":
+            return SimpleReport.generate(data)
+        return CompleteReport.generate(data)
