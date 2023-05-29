c26b672c9a0ffa62351d2773c001005c6e4622e1
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 05:25:54 -0300

    req 8 - test_product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "MESA",
+        "Forces of Nature",
+        "2022-05-04",
+        "2023-02-09",
+        "FR48",
+        "Conservar ao abrigo de luz",
+    )
+
+    assert produto.__repr__() == (
+        "O produto MESA fabricado em 2022-05-04 por "
+        "Forces of Nature com validade até 2023-02-09 "
+        "precisa ser armazenado Conservar ao abrigo de luz."
+    )
+
+    #     "id": 1,
+    # "nome_do_produto": "MESA",
+    # "nome_da_empresa": "Forces of Nature",
+    # "data_de_fabricacao": "2022-05-04",
+    # "data_de_validade": "2023-02-09",
+    # "numero_de_serie": "FR48",
+    # "instrucoes_de_armazenamento": "Conservar ao abrigo de luz"
