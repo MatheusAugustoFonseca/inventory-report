8654f080aab816d8efaf6cb9f8fd5c70f7cc93f1
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-05-29 03:08:08 -0300

    req 4 e 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -10,6 +11,9 @@ class Inventory:
             with open(file_path, encoding="utf8") as csv_file:
                 csv_reader = csv.DictReader(csv_file)
                 data = [row for row in csv_reader]
+        if ".json" in file_path:
+            with open(file_path) as json_file:
+                data = json.load(json_file)
         if report_type == "simples":
             return SimpleReport.generate(data)
         return CompleteReport.generate(data)
